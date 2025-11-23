d3a1f6785798a5b99f1bf59d38bb1d47538a4e0d
Rezamaulidan <fauzanngrh003@gmail.com>
2025-11-23 08:56:28 +0700

    perbaikan daftar kamar

--- resources/views/edit_data_kamar.blade.php
+++ resources/views/edit_data_kamar.blade.php
@@ -9,7 +9,7 @@
     {{-- Aset yang Diperlukan --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/style_info_kamar.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/style_info_kamar.css') }}"> -->
 
     <style>
     body {
